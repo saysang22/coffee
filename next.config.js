3ede6859976b9790ae92ed08ae9299b1418c4133
MinSeo <saysang2212@gmail.com>
2023-06-25 19:11:53 +0900

    CI/CD테스트중

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const repository = "https://saysang22.github.io/coffee/";
+// const repository = "https://saysang22.github.io/coffee/";
 const nextConfig = {
   output: "export",
-  images: {
-    loader: "custom",
-    loaderFile: "./app/image.ts",
-  },
 };
 
 module.exports = nextConfig;
