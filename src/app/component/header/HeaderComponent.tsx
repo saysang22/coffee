e75329825607840e43c93298849bf65d93bc3494
MinSeo <saysang2212@gmail.com>
2023-06-26 14:07:02 +0900

    배포테스트13

--- src/app/component/header/HeaderComponent.tsx
+++ src/app/component/header/HeaderComponent.tsx
@@ -23,31 +23,33 @@ const HeaderComponent: React.FC<{ data: HeaderItme[] }> = ({ data }) => {
   getSession();
 
   return (
-    <div className={style.wrap}>
-      <div className={style.inner}>
-        <div className={style.left}>
-          <div className={style.logo}>민하코</div>
-          <HeaderList data={data} />
-        </div>
-        <div className={style.rigth}>
-          <HeaderInput />
-          <div className={style.info_wrap}>
-            {session === null ? (
-              <Login />
-            ) : (
-              <>
-                <Logout />
-                <span>{session && session.user.name}님</span>
-              </>
-            )}
-            <Cart />
+    <>
+      <header className={style.wrap}>
+        <div className={style.inner}>
+          <div className={style.left}>
+            <div className={style.logo}>민하코</div>
+            <HeaderList data={data} />
+          </div>
+          <div className={style.rigth}>
+            <HeaderInput />
+            <div className={style.info_wrap}>
+              {session === null ? (
+                <Login />
+              ) : (
+                <>
+                  <Logout />
+                  <span>{session && session.user.name}님</span>
+                </>
+              )}
+              <Cart />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={style.mobile}>
-        <MobileHeader />
-      </div>
-    </div>
+        <div className={style.mobile}>
+          <MobileHeader />
+        </div>
+      </header>
+    </>
   );
 };
 
