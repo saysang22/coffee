380d7ee08a517a38c3a644e2f015879ebc9aa1da
MinSeo <saysang2212@gmail.com>
2023-06-26 13:07:03 +0900

    '배포테스트다7'

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 // const repository = "https://saysang22.github.io/coffee/";
 const nextConfig = {
   basePath: "/coffee",
-  assetPrefix: "/coffee",
   output: "export",
+  exportTrailingSlash: true,
   experimental: {
     appDir: true,
   },
