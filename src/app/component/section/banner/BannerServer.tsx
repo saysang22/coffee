2dddd9a8006989a91ec094ebed769cd200f6267f
MinSeo <saysang2212@gmail.com>
2023-07-05 00:09:03 +0900

    '수정'

--- src/app/component/section/banner/BannerServer.tsx
+++ src/app/component/section/banner/BannerServer.tsx
@@ -3,7 +3,8 @@ import Banner from "./Banner";
 import { getServerSession } from "next-auth";
 
 const BannerServer = async () => {
-  // return <>{session === null ? <Banner /> : null}</>;
+  let session = await getServerSession();
+  return <>{session === null ? <Banner /> : null}</>;
 };
 
 export default BannerServer;
