df7cbdfa7b0a69e7d3aa84708e0649f25f6f15ad
MinSeo <saysang2212@gmail.com>
2023-07-05 00:18:48 +0900

    '되돌리는중'

--- src/app/component/section/banner/BannerServer.tsx
+++ src/app/component/section/banner/BannerServer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Banner from "./Banner";
 import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const BannerServer = async () => {
-  let session = await getServerSession();
+  let session = await getServerSession(authOptions);
   return <>{session === null ? <Banner /> : null}</>;
 };
 
